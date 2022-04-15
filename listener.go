6e7a640b9df2fecf044688037406932ab460c2c6
wenlong <y1423027101@gmail.com>
2022-04-15 14:52:03 +0800

    clean

--- listener.go
+++ listener.go
@@ -2,15 +2,17 @@ package trojan
 
 import (
 	"bufio"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
 	"io"
 	"net"
 	"net/textproto"
 	"os"
-
-	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
-	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
+	"strings"
 
 	"go.uber.org/zap"
 
@@ -19,6 +21,7 @@ import (
 )
 
 func init() {
+	userLists = make([]string, 0)
 	caddy.RegisterModule(ListenerWrapper{})
 	httpcaddyfile.RegisterDirective("trojan_gfw", func(h httpcaddyfile.Helper) ([]httpcaddyfile.ConfigValue, error) {
 		return []httpcaddyfile.ConfigValue{{
@@ -28,15 +31,27 @@ func init() {
 	})
 }
 
+var userLists []string
+
+func validateUser(user string) bool {
+	if len(user) != trojan.HeaderLen {
+		return false
+	}
+	for _, list := range userLists {
+		if list == user {
+			return true
+		}
+	}
+	return false
+}
+
 // ListenerWrapper implements an TLS wrapper that it accept connections
 // from clients and check the connection with pre-defined password
-// and aead cipher defined by go-shadowsocks2, and return a normal page if
-// failed.
+// and return a normal page if failed.
 type ListenerWrapper struct {
-	// Upstream is ...
-	Upstream *Upstream `json:"-,omitempty"`
 	// Logger is ...
-	Logger *zap.Logger `json:"-,omitempty"`
+	Logger    *zap.Logger `json:"-,omitempty"`
+	UserLists []string    `json:"users"`
 }
 
 // CaddyModule returns the Caddy module information.
@@ -50,21 +65,62 @@ func (ListenerWrapper) CaddyModule() caddy.ModuleInfo {
 // Provision implements caddy.Provisioner.
 func (m *ListenerWrapper) Provision(ctx caddy.Context) error {
 	m.Logger = ctx.Logger(m)
-	m.Upstream = NewUpstream(ctx.Storage(), m.Logger)
 	return nil
 }
 
 // WrapListener implements caddy.ListenWrapper
 func (m *ListenerWrapper) WrapListener(l net.Listener) net.Listener {
-	ln := NewListener(l, m.Upstream, m.Logger)
+	ln := NewListener(l, m.Logger)
 	go ln.loop()
 	return ln
 }
 
 // UnmarshalCaddyfile unmarshals Caddyfile tokens into h.
-func (*ListenerWrapper) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+func (m *ListenerWrapper) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	if !d.Next() {
+		return d.ArgErr()
+	}
+	args := d.RemainingArgs()
+	if len(args) > 0 {
+		return d.ArgErr()
+	}
+	for nesting := d.Nesting(); d.NextBlock(nesting); {
+		subdirective := d.Val()
+		args := d.RemainingArgs()
+		switch subdirective {
+		case "user":
+			if len(args) < 1 {
+				return d.ArgErr()
+			}
+			for _, v := range args {
+				if len(v) == 0 {
+					return d.Err("empty user is not allowed")
+				}
+				b := sha256.Sum224([]byte(v))
+				var sum224 []byte = make([]byte, sha256.Size224)
+				copy(sum224, b[:])
+				newUser := hex.EncodeToString(sum224)
+				userLists = append(userLists, newUser)
+				m.UserLists = append(m.UserLists, v)
+			}
+		}
+	}
 	return nil
 }
+
+func (m *ListenerWrapper) ValidateUser(user string) bool {
+	if len(user) != trojan.HeaderLen {
+		return false
+	}
+	for _, list := range userLists {
+		if list == user {
+			return true
+		}
+	}
+	return false
+}
+
+type UserValidateFunc func(string) bool
 
 // Interface guards
 var (
@@ -79,8 +135,6 @@ type Listener struct {
 
 	// Listener is ...
 	net.Listener
-	// Upstream is ...
-	Upstream *Upstream
 	// Logger is ...
 	Logger *zap.Logger
 
@@ -91,10 +145,9 @@ type Listener struct {
 }
 
 // NewListener is ...
-func NewListener(ln net.Listener, up *Upstream, logger *zap.Logger) *Listener {
+func NewListener(ln net.Listener, logger *zap.Logger) *Listener {
 	l := &Listener{
 		Listener: ln,
-		Upstream: up,
 		Logger:   logger,
 		conns:    make(chan net.Conn, 8),
 		closed:   make(chan struct{}),
@@ -137,7 +190,7 @@ func (l *Listener) loop() {
 			continue
 		}
 
-		go func(c net.Conn, lg *zap.Logger, up *Upstream) {
+		go func(c net.Conn, lg *zap.Logger) {
 			// behave like a normal http server made by golang
 			// https://github.com/golang/go/blob/19309779ac5e2f5a2fd3cbb34421dafb2855ac21/src/net/http/request.go#L1037
 			r := bufio.NewReaderSize(c, trojan.HeaderLen)
@@ -149,7 +202,7 @@ func (l *Listener) loop() {
 				return
 			}
 
-			if !validateLine(line, up) {
+			if !validateUser(strings.TrimSpace(line)) {
 				lg.Error(fmt.Sprintf("invalid header: %s", line))
 				l.conns <- utils.RewindConn(c, r, line+"\r\n")
 				return
@@ -164,11 +217,8 @@ func (l *Listener) loop() {
 			if err != nil {
 				lg.Error(fmt.Sprintf("handle net.Conn error: %v", err))
 			}
-			up.Consume(line, nr, nw)
-		}(conn, l.Logger, l.Upstream)
+			// prometheus
+			_, _ = nr, nw
+		}(conn, l.Logger)
 	}
-}
-
-func validateLine(line string, up *Upstream) bool {
-	return len(line) == trojan.HeaderLen && up.Validate(line[:trojan.HeaderLen])
 }
